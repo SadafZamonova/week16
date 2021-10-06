836eba0add3937031f84225770450817626bf162
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-10-06 16:08:00 +0500

    2

--- second/index.js
+++ second/index.js
@@ -1,3 +1,21 @@
-let color = document.querySelector("#color");
+let selectors = document.querySelectorAll("select");
+let clickButton = document.querySelector(".button");
+
+
+function onClick() {
+  let result = 0;
+  
+  for (let i = 0; i < 3 ; i++) {
+    let num = +selectors[i].value;
+     result += num ;
+  }
+
+  let input = +document.querySelectorAll("input").value;
+  let resultNode = document.querySelector(".result");
+  resultNode.textContent=result + input;
+
+}
+
+clickButton.addEventListener("click", onClick);
 
  
